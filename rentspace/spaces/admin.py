11df22268ba28e7de30ae4b17abbc1bb67e6a6cd
Tatenda  Katema <primewebwizards@gmail.com>
2026-09-13 14:15:39 +0200

    adding categories and amenities

--- rentspace/spaces/admin.py
+++ rentspace/spaces/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Space, SpaceType, Location, SpaceImage
+from .models import Space, SpaceType, Location, SpaceImage, Amenity, Category
 
 # Register your models here.
 admin.site.register(Space)
 admin.site.register(SpaceType)
 admin.site.register(Location)
 admin.site.register(SpaceImage)
+admin.site.register(Amenity)
+admin.site.register(Category)
